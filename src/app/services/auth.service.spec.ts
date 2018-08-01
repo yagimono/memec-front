28c74a1f8f6d1ca16f6f5fc9f33d0e565809e658
yagimono <yagi@monosense.co.jp>
2018-08-01 16:57:42 +0900

    認証apiのモックテスト実装

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,15 +1,54 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
+import {defer} from 'rxjs/observable/defer';
+import {AuthService} from './auth.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
-import { AuthService } from './auth.service';
+// Subscribeされたときに指定のデータを返却するObservableを生成
+function asyncData<T>(data: T) {
+    return defer(() => Promise.resolve(data));
+}
 
-xdescribe('AuthService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AuthService]
+function asyncError<T>(errorObject: any) {
+    return defer(() => Promise.reject(errorObject));
+}
+
+describe('AuthService', () => {
+    let httpClientSpy: { put: jasmine.Spy };
+    let authService: AuthService;
+
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['put']);
+        authService = new AuthService(<any> httpClientSpy);
+        TestBed.configureTestingModule({
+            providers: [
+                {provide: AuthService, useValue: authService}
+            ]
+        });
     });
-  });
 
-  it('should be created', inject([AuthService], (service: AuthService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('207が返ってきた場合、認証成功', inject([AuthService], (service: AuthService) => {
+        const expected: any = [{code: 7, error: 'Authorization Succeeded: AKB Group Shop\nAuthorization Failed: Nihombashira', traces: []}];
+
+        httpClientSpy.put.and.returnValue(asyncData(expected));
+
+        authService.auth('T2015110201', '00000001').subscribe(
+            response => expect(response).toEqual(expected, 'expected'),
+            fail
+        );
+        expect(httpClientSpy.put.calls.count()).toBe(1, 'one call');
+    }));
+
+    it('401が返ってきた場合、認証失敗', () => {
+        const errorResponse = new HttpErrorResponse({
+            error: 'test 401 error',
+            status: 401, statusText: 'Unauthorized'
+        });
+
+        httpClientSpy.put.and.returnValue(asyncError(errorResponse));
+
+        authService.auth('T2015110201', '00000000').subscribe(
+            heroes => fail('expected an error'),
+            error  => expect(error.message).toContain('401 Unauthorized')
+        );
+    });
 });
